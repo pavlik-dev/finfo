b0c3a623a50300afa4fcccae4709e534767ce586
Pavlik <pavlik@pavliks-macbookpro>
2025-02-23 03:54:43 +0200

    Removed extension support; changed Fields a bit; added mount point field for character/block devices.

--- Field.cpp
+++ Field.cpp
@@ -1,87 +1,42 @@
 // Field.cpp
 #ifndef FIELD_CPP
 #define FIELD_CPP
-#define TABS "  "
+#define TABS "  "  //Change this however you want
 
-#include <algorithm>
 #include <iostream>
-#include <map>
+#include <vector>
 #include <string>
-#include "exception.cpp"
 
 using namespace std;
-
-// #pragma region Exceptions
-
-class DuplicateIDException {
-private:
-	string _what;
-public:
-	DuplicateIDException(string what) : _what(what) {}
-	string what() {
-		return this->_what;
-	}
-};
-
-// #pragma endreg
 
 class Field
 {
 public:
-	string id = "";  // can be empty ig
 	string name = "";
 	string value = "";
-	map<string, Field> subfields;
-	bool colon = true;
-	bool visible = true; // if false, just ignore it and move on
-
+	vector<Field> subfields;
 
 	// Constructors
-	Field(std::string id, std::string n, std::string v, bool c = true):
-		id(id), name(n), value(v), colon(c) {}
-	//Field(std::string n, std::string v, bool c = true):
-	//	name(n), value(v), colon(c) {}
-	// const Field no_id(std::string n, std::string v, bool c = true) {
-	// 	return Field("", n, v, c);
-	// }
+	Field(std::string n, std::string v):
+		name(n), value(v) {}
 
-	Field() {}
-
-	// tabulation - specifies the level of in(d/t??)ent used
+	// tabulation - specifies the level of indent used
 	// show_id - showes field's id instead of name
-	void print(int tabulation = 0, bool show_id = false, string id_pre = "") {
-		if (!this->visible) return;
+	void print(int tabulation = 0) {
 		for (int i = 0; i < tabulation; ++i)
 			cout << TABS;
-		if (!show_id) cout << this->name;
-		else cout << id_pre << this->id;
-		if (this->colon) cout << ":";
+		cout << this->name << ":";
 		if (this->value != "") cout << " " << this->value;
 		cout << endl;
 
-		for (auto& field : this->subfields)
-			field.second.print(tabulation+1, show_id, id_pre+this->id+".");
+		for (size_t i = 0; i < this->subfields.size(); ++i)
+            this->subfields[i].print(tabulation+1);
 		return;
 	}
 
 	void add_field(Field subfield) {
-		if (this->subfields.count(subfield.id) > 0) {
-			throw new DuplicateIDException(subfield.id);
-		}
-		this->subfields[subfield.id] = subfield;
+		this->subfields.push_back(subfield);
 	}
 };
-
-// void print_fields(vector<Field> fields, int tabulation);
-
-// void print_field(Field field, int tabulation = 0) {
-	
-// }
-
-// void print_fields(vector<Field> fields, int tabulation = 0) {
-// 	for (const auto& field : fields)
-// 		print_field(field, tabulation);
-// 	return;
-// }
 
 #endif
--- main.cpp
+++ main.cpp
@@ -1,35 +1,28 @@
+#include <cstdio>
 #define TABS "  "
-#define FIELD_DBG 0
-#define LOAD_EXT_VERB 0
 
-#include <iostream>
-#include <string>
-#include <vector>
-#include <memory>
-#include <dirent.h>
 #include <array>
-#include <sys/stat.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <pwd.h>
+#include <dirent.h>
 #include <dlfcn.h>
-#include <map>
-#include <iterator>  // for back_inserter
+#include <iostream>
+#include <pwd.h>
+#include <string>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <vector>
+
+#include <mntent.h>
 
 using namespace std;
 
 #include "Field.cpp"
-#include "extension_load.cpp"
 #include "file.cpp"
 
-bool field_dbg = FIELD_DBG;
-bool load_ext_verb = LOAD_EXT_VERB;
-
-std::string trim(const std::string& str,
-                 const std::string& whitespace = " \t\n")
+string trim(const string &str, const string &whitespace = " \t\n")
 {
     const auto strBegin = str.find_first_not_of(whitespace);
-    if (strBegin == std::string::npos)
+    if (strBegin == string::npos)
         return ""; // no content
 
     const auto strEnd = str.find_last_not_of(whitespace);
@@ -38,336 +31,239 @@ std::string trim(const std::string& str,
     return str.substr(strBegin, strRange);
 }
 
-std::string dirnameOf(const std::string& fname)
+string dirnameOf(const string &fname)
 {
-  size_t pos = fname.find_last_of("\\/");
-  return (std::string::npos == pos)
-    ? ""
-    : fname.substr(0, pos);
+    size_t pos = fname.find_last_of("\\/");
+    return (string::npos == pos) ? "" : fname.substr(0, pos);
 }
 
-inline string get_extension(const string& filename) {
-  return filename.substr(filename.find_last_of(".") + 1);
+inline string get_extension(const string &filename)
+{
+    return filename.substr(filename.find_last_of(".") + 1);
 }
 
 // Source: https://stackoverflow.com/a/12774387
 // Yeah I stole this from SO :P
-inline bool file_exists(const string& name) {
-  struct stat buffer;
-  return lstat(name.c_str(), &buffer) == 0;
-}
-
-array<int, 4> get_files_in_directory(const string& path)
+inline bool file_exists(const string &name)
 {
-  DIR* dp = opendir(path.c_str());
-  // int normal, hidden;
-  array<int, 2> normal = {0, 0};
-  array<int, 2> hidden = {0, 0};
-  if (dp != nullptr) {
-    struct dirent* ep;
-    while((ep = readdir(dp))) {
-      string name(ep->d_name);
-      if (name != "." && name != "..") {
-        if (name[0] == '.') {
-          hidden[(ep->d_type == DT_DIR) ? 1 : 0]++;
-        }else {
-          normal[(ep->d_type == DT_DIR) ? 1 : 0]++;
+    struct stat buffer;
+    return lstat(name.c_str(), &buffer) == 0;
+}
+
+array<int, 4> get_files_in_directory(const string &path)
+{
+    DIR *dp = opendir(path.c_str());
+    // int normal, hidden;
+    array<int, 2> normal = {0, 0};
+    array<int, 2> hidden = {0, 0};
+    if (dp != nullptr)
+    {
+        struct dirent *ep;
+        while ((ep = readdir(dp)))
+        {
+            string name(ep->d_name);
+            if (name != "." && name != "..")
+            {
+                if (name[0] == '.')
+                {
+                    hidden[(ep->d_type == DT_DIR) ? 1 : 0]++;
+                }
+                else
+                {
+                    normal[(ep->d_type == DT_DIR) ? 1 : 0]++;
+                }
+            }
         }
-      }
+
+        (void)closedir(dp);
     }
-
-    (void)closedir(dp);
-  }
-  else
-    perror("Couldn't open the directory");
-  array<int, 4> result;
-  result[0] = normal[0];
-  result[1] = normal[1];
-  result[2] = hidden[0];
-  result[3] = hidden[1];
-  return result;
+    else
+        perror("Couldn't open the directory");
+    array<int, 4> result;
+    result[0] = normal[0];
+    result[1] = normal[1];
+    result[2] = hidden[0];
+    result[3] = hidden[1];
+    return result;
 }
 
-inline string basename(string path, bool is_dir=false) {
-  return (path.substr(path.find_last_of("/") + 1))+((is_dir) ? "/" : "");
+inline string basename(string path, bool is_dir = false)
+{
+    return (path.substr(path.find_last_of("/") + 1)) + ((is_dir) ? "/" : "");
 }
 
-string print_permissions(struct stat _stat) {
-  mode_t mode = _stat.st_mode;
-  bool is_dir = _stat.st_mode & S_IFDIR;
-  string user;
-  user += (mode & S_IRUSR) ? "r" : "-";
-  user += (mode & S_IWUSR) ? "w" : "-",
-  user += (mode & S_IXUSR) ? "x" : "-";
+string print_permissions(struct stat _stat)
+{
+    mode_t mode = _stat.st_mode;
+    bool is_dir = _stat.st_mode & S_IFDIR;
+    string user;
+    user += (mode & S_IRUSR) ? "r" : "-";
+    user += (mode & S_IWUSR) ? "w" : "-", user += (mode & S_IXUSR) ? "x" : "-";
 
-  string group;
-  group += (mode & S_IRGRP) ? "r" : "-";
-  group += (mode & S_IWGRP) ? "w" : "-";
-  group += (mode & S_IXGRP) ? "x" : "-";
+    string group;
+    group += (mode & S_IRGRP) ? "r" : "-";
+    group += (mode & S_IWGRP) ? "w" : "-";
+    group += (mode & S_IXGRP) ? "x" : "-";
 
-  string other;
-  other += (mode & S_IROTH) ? "r" : "-";
-  other += (mode & S_IWOTH) ? "w" : "-";
-  other += (mode & S_IXOTH) ? "x" : "-";
+    string other;
+    other += (mode & S_IROTH) ? "r" : "-";
+    other += (mode & S_IWOTH) ? "w" : "-";
+    other += (mode & S_IXOTH) ? "x" : "-";
 
-  return (is_dir ? "d" : "-") + user + group + other;
+    return (is_dir ? "d" : "-") + user + group + other;
 }
 
-string get_file_type(struct stat _stat) {
-  switch (_stat.st_mode & S_IFMT) {
-    case S_IFBLK:  return("block device");     break;
-    case S_IFCHR:  return("character device"); break;
-    case S_IFDIR:  return("directory/folder"); break;
-    case S_IFIFO:  return("FIFO/pipe");        break;
-    case S_IFLNK:  return("symlink");          break;
-    case S_IFREG:  return("regular file");     break;
-    case S_IFSOCK: return("socket");           break;
-    default:       return("unknown");          break;
-   }
+string get_file_type(struct stat _stat)
+{
+    switch (_stat.st_mode & S_IFMT)
+    {
+    case S_IFBLK:
+        return ("block device");
+    case S_IFCHR:
+        return ("character device");
+    case S_IFDIR:
+        return ("directory/folder");
+    case S_IFIFO:
+        return ("FIFO/pipe");
+    case S_IFLNK:
+        return ("symlink");
+    case S_IFREG:
+        return ("regular file");
+    case S_IFSOCK:
+        return ("socket");
+    default:
+        return ("unknown");
+    }
 }
 
 string readable_fs(const long int size /*in bytes*/,
-      const double divide_by = 1024,
-      const string& suffix = "B")
+                                     const double divide_by = 1024, const string &suffix = "B")
 {
-  if(size < 1024) {
-  char buffer[128];
-  sprintf(buffer, "%i B", static_cast<int>(size));
-  string result(buffer);
-  return buffer;
-  }
-  auto result = static_cast<double>(size);
-  int i = 0;
-  const string units[] = {"", "K", "M", "G", "T", "P", "E", "Z", "Y"};
-  while (result > divide_by) {
-  result /= divide_by;
-  i++;
-  }
-  char buffer[128];
-  sprintf(buffer, "%.*f %s%s", i, result, units[i].c_str(), suffix.c_str());
-  string str_result(buffer);
-  return str_result;
+    if (size < 1024)
+    {
+        char buffer[128];
+        sprintf(buffer, "%i B", static_cast<int>(size));
+        string result(buffer);
+        return buffer;
+    }
+    auto result = static_cast<double>(size);
+    int i = 0;
+    const string units[] = {"", "K", "M", "G", "T", "P", "E", "Z", "Y"};
+    while (result > divide_by)
+    {
+        result /= divide_by;
+        i++;
+    }
+    char buffer[128];
+    sprintf(buffer, "%.*f %s%s", i, result, units[i].c_str(), suffix.c_str());
+    string str_result(buffer);
+    return str_result;
 }
 
-int print_usage(char* argv[]) {
-  cerr << "Usage:" << endl;
-  cerr << TABS << argv[0] << " [args] <files>" << endl;
-  cerr << "Args:" << endl;
-  cerr << TABS << "-fd" << " Prints out field's ID instead of it's name." << endl;
-  cerr << TABS << "-ev" << " Prints out every loaded extension." << endl;
-  return 1;
+int print_usage(char *argv[])
+{
+    cerr << "Usage:" << endl;
+    cerr << TABS << argv[0] << " <files>" << endl;
+    return 1;
 }
 
 int main(int argc, char *argv[])
 {
-  if (argc < 2) {
-    return print_usage(argv);
-  }
-  vector<string> files;
-  vector<string> args;
+    if (argc < 2)
+        return print_usage(argv);
 
-  bool files_started = false;
-  string stop_args = "--";
-  for (int i = 1; i < argc; ++i) {
-    if (argv[i][0] == '-' && !files_started) {
-      if (argv[i] != stop_args)
-        args.push_back(string(argv[i]));
-      else
-        files_started = true;
-    } else {
-      files.push_back(string(argv[i]));
-    }
-  }
+    vector<string> files;
+    for (int i = 1; i < argc; ++i)
+        files.push_back(string(argv[i]));
 
-  if (files.size() < 1) {
-    return print_usage(argv);
-  }
-
-  const string field__dbg = "-fd";
-  const string ext__dbg = "-ev";
-  for (const string& arg : args) {
-    
-    if (arg == field__dbg) {
-      field_dbg = true;
-    }
-    if (arg == ext__dbg) {
-      load_ext_verb = true;
-    }
-  }
-
-  vector<shared_ptr<Extension>> extensions;
-
-  char* temp = (char*) malloc(4096);
-  readlink("/proc/self/exe", temp, 4096);
-  const string exec_path(temp);
-  // delete[] temp;
-  free(temp);
-
-  const string extloc = dirnameOf(exec_path)+"/exts/";
-  if (file_exists(extloc)) {
-    DIR *dir;
-    struct dirent *ent;
-    if ((dir = opendir (extloc.c_str())) != NULL) {
-      /* print all the files and directories within directory */
-      while ((ent = readdir (dir)) != NULL) {
-        string fn = extloc+ent->d_name;
-        if (get_extension(fn) != "ext") continue;
-        if (load_ext_verb) cout << "Loading " << fn << endl;
-        shared_ptr<Extension> extension;
-        try { extension = load_extension(fn); }
-        catch (Exception& e) {
-          cerr << "Error in " << fn << ": " << e.what() << endl;
-          continue;
-        }
-        if (extension == nullptr) {
-          cerr << "Failed to load extension " << fn << ", skip." << endl;
-          continue;
-        }
-        // extension->file_name = fn;
-        if (load_ext_verb) cout << "Loaded " << fn << " successfully." << endl;
-        extensions.push_back(extension);
-      }
-      closedir (dir);
-    } else {
-      /* could not open directory */
-      perror ("");
-      return EXIT_FAILURE;
-    }
-  }
-
-  // Compatibility 
-
-  bool first = true;
-  int counter = 0;
-  for (const string& file : files) {
-    if (!first) {
-      cout << endl;
-    }
-    counter++;
-    File file_obj(file, Stat::LSTAT);
-    struct stat file_stat = file_obj.file_stat;
-    string abspath = file_obj.abs_path;
-
-    vector<shared_ptr<Extension>> temp_ext;
-    temp_ext.insert(temp_ext.begin(), extensions.begin(), extensions.end());
-
-    temp_ext.erase(
-      std::remove_if(temp_ext.begin(), temp_ext.end(),
-        [&](const std::shared_ptr<Extension>& ext) {
-        return !ext->is_compatible(abspath);
-      }),
-      temp_ext.end()
-    );
-
-
-    bool is_dir = file_stat.st_mode & S_IFDIR;
-
-    /* Using extensions only after we made sure that the file exists and is valid */
-
-    vector<Field> int_fields;
-    vector<Field> ext_fields;
-    for (const auto& ext : temp_ext) {
-      // if (ext_fields.count(ext->ext_id) != 0) {
-      //   cerr << ext->file_name << " has duplicate ID " << ext->ext_id << ". Skip." << endl;
-      // }
-      Field info;
-      try {
-        info = ext->get_info(abspath);
-        info.id = ext->ext_id;
-      } catch (Exception& e) {
-        info = Field(ext->ext_id, "Error in"+ext->ext_id, e.what());
-      } catch (exception& e) {
-        info = Field(ext->ext_id, "Error in "+ext->ext_id, e.what());
-      } catch (...) {
-        info = Field(ext->ext_id, "Error in "+ext->ext_id, "*unknown error*");
-      }
-      ext_fields.push_back(info);
-    }
-
-    int_fields.push_back(Field("type", "Type", get_file_type(file_stat)));
-
-    if (is_dir) {
-      Field dircont("contents", "Contents", "");
-      int buffer_size = 128;
-      auto insides = get_files_in_directory(file);
-      char *buffer = (char*)malloc(buffer_size);
-      if (buffer == NULL) {
-          // Check if malloc failed
-          fprintf(stderr, "Memory allocation failed\n");
-          return 1;
-      }
-      snprintf(buffer, buffer_size, "%i files (+%i hidden)",
-          insides[0], insides[1]);
-      string _str_files = buffer;
-      free(buffer);
-
-      buffer = (char*)malloc(buffer_size);
-      if (buffer == NULL) {
-          // Check if malloc failed
-          fprintf(stderr, "Memory allocation failed\n");
-          return 1;
-      }
-      snprintf(buffer, buffer_size, "%i folders (+%i hidden)",
-          insides[2], insides[3]);
-      string _str_dirs = buffer;
-      Field files("files", _str_files, "", false);
-      Field dirs("dirs", _str_dirs, "", false);
-      dircont.add_field(files);
-      dircont.add_field(dirs);
-      int_fields.push_back(dircont);
-      free(buffer);
-    } else {
-      int_fields.push_back(Field("size", "Size",
-          readable_fs((long)file_stat.st_size)));
-    }
-
-    char buffer[8];
-    snprintf(buffer, sizeof(buffer), " (%3o)", file_stat.st_mode & 0777);
-    string _perms(buffer);
-    int_fields.push_back(Field("permissions", "Permissions",
-        print_permissions(file_stat)+_perms));
-
-    struct tm * timeinfo;
-    Field dates("dates", "Last", "");
-    #ifdef __APPLE__ // macOS
-      timeinfo = localtime(&file_stat.st_birthtime); // or gmtime() depending on what you want
-      dates.add_field(Field("creation_date", "Creation date", trim(string(asctime(timeinfo)))));
-    #endif
-
-    timeinfo = localtime(&file_stat.st_atime);
-    dates.add_field(Field("open_date", "opened", trim(string(asctime(timeinfo)))));
-
-    timeinfo = localtime(&file_stat.st_mtime);
-    dates.add_field(Field("modify_date", "modified", trim(string(asctime(timeinfo)))));
-
-    int_fields.push_back(dates);
-
-    Field start;
-    start.name = "\x1b[1m"+basename(abspath, is_dir)+"\x1b[0m";
-    start.id = "start";
-
-    for (auto& fld : int_fields)
+    bool first = true;
+    int counter = 0;
+    for (const string &file : files)
     {
-      try {
-        start.add_field(fld);
-      } catch (DuplicateIDException& e) {
-        cerr << TABS << "Duplicate ID: " << e.what() << endl;
-      }
-    }
-    for (auto& fld : ext_fields)
-    {
-      try {
-        start.add_field(fld);
-      } catch (DuplicateIDException* e) {
-        cerr << TABS << "Duplicate ID: " << e->what() << endl;
-        cerr << TABS << "Please delete that extension, or modify it and try again." << endl;
-      }
-    }
+        if (!first)
+            cout << endl;
+        counter++;
+        File file_obj(file, Stat::LSTAT);
+        struct stat file_stat = file_obj.file_stat;
+        string abspath = file_obj.abs_path;
 
-    string file_id = "file" + to_string(counter) + ".";
+        string file_type = get_file_type(file_stat);
 
-    start.print(0, field_dbg, file_id);
-    first = false;
-  }
-  return 0;
+        bool is_dir = file_type == "directory/folder";
+        bool is_file = file_type == "regular file";
+        bool is_device =
+                file_type == "block device" || file_type == "character device";
+
+        vector<Field> fields;
+
+        fields.push_back(Field("Type", file_type));
+
+        if (is_dir)
+        {
+            Field dircont("Contents", "");
+            auto insides = get_files_in_directory(file);
+
+            string filesText = to_string(insides[0]) + " files (+" +
+                                                 to_string(insides[1]) + " hidden)";
+            string dirsText = to_string(insides[2]) + " folders (+" +
+                                                to_string(insides[3]) + " hidden)";
+
+            Field files(filesText, "");
+            Field dirs(dirsText, "");
+
+            dircont.add_field(files);
+            dircont.add_field(dirs);
+            fields.push_back(dircont);
+        }
+        else if (is_file)
+        {
+            fields.push_back(Field("Size", readable_fs((long)file_stat.st_size)));
+        }
+        else if (is_device)
+        {
+            struct mntent *ent;
+            FILE *aFile;
+
+            aFile = setmntent("/proc/mounts", "r");
+            if (aFile == NULL) {
+                perror("setmntent");
+                fields.push_back(Field("Mount point: ", "N/A"));
+            } else {
+                while (NULL != (ent = getmntent(aFile)))
+                    if (ent->mnt_fsname == abspath)
+                        fields.push_back(Field("Mount point", ent->mnt_dir));
+                endmntent(aFile);
+            }
+        }
+
+        char buffer[8];
+        snprintf(buffer, sizeof(buffer), " (%3o)", file_stat.st_mode & 0777);
+        string _perms(buffer);
+        fields.push_back(
+                Field("Permissions", print_permissions(file_stat) + _perms));
+
+        struct tm *timeinfo;
+        Field dates("Last", "");
+#ifdef __APPLE__ // macOS
+        timeinfo = localtime(&file_stat.st_birthtime);
+        dates.add_field(Field("Creation date", trim(string(asctime(timeinfo)))));
+#endif
+
+        timeinfo = localtime(&file_stat.st_atime);
+        dates.add_field(Field("opened", trim(string(asctime(timeinfo)))));
+
+        timeinfo = localtime(&file_stat.st_mtime);
+        dates.add_field(Field("modified", trim(string(asctime(timeinfo)))));
+
+        fields.push_back(dates);
+
+        Field start("\x1b[1m" + basename(abspath, is_dir) + "\x1b[0m", "");
+
+        for (size_t i = 0; i < fields.size(); ++i)
+            start.add_field(fields[i]);
+
+        start.print();
+        first = false;
+    }
+    return 0;
 }
